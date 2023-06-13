3113b84a05b1da6ebf044cccde35ac80fa34e359
MH RONY <78216965+dev-mhrony@users.noreply.github.com>
2023-06-13 16:27:31 +0600

    Update dbconnection.php

--- doctor/includes/dbconnection.php
+++ doctor/includes/dbconnection.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','damsmsdb');
+define('DB_NAME','ccbd_dams');
 // Establish database connection.
 try
 {
